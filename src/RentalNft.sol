ab277949be577dd0738a0a894cff38f3f1f76221
kirube1992 <kirubelaynalem@gmail.com>
2024-04-24 10:39:37 -0300

    creat-rentalNft

--- src/RentalNft.sol
+++ src/RentalNft.sol
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
 interface IRentalNFT is IERC721 {
 struct UserInfo {
 address user;
